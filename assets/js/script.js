0a31ce05ac1dd44961a54482b0693ea44d7f9dbb
Andreas Hoffmann <44086533+Hoffydox@users.noreply.github.com>
2020-03-30 18:17:32 +0200

    updated 3 i 1 WP

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,18 @@ const WPkey = 'ZojeMhiQCU4ZzrmgoT4Bx8HkDlV2ABXM'; // Andreas WP API Nøgle
 
 let appOptions;
 const postInfoId = 5;
-const førstetInfoPost = 58;
-const andenInfoPost = 69;
-// parameter test neden for
-randompost = 69;
-var TPtest = randompost;
-getDataWP(TPtest); //parameter der kan skifte posts ud efter en cycle? postene skal nok være i et array som looper?
 
-function getDataWP(TPtest){
+const InfoPost = 58;
+const midPost = 69;
+const rightPost = 99;
+
+// parameter test neden for
+randompost = 58;
+var TPtest = randompost;
+
+getDataWP(); //parameter der kan skifte posts ud efter en cycle? postene skal nok være i et array som looper?
+
+function getDataWP(){
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
         if (this.readyState == 4 && this.status == 200){
@@ -25,13 +29,13 @@ function getDataWP(TPtest){
                 
           // console.log(data.date);
            // console.log(data.explanation);
-            // fører koden videre til en function ved navn renderWP
-            renderWP(data);
+            // fører koden videre til en function ved navn renderInfoWP
+            renderInfoWP(data);
            
         }
     }
     // xhttp.open('GET', `${WPurl}posts/?tags=${postInfoId}`, true);
-    xhttp.open('GET', `${WPurl}posts/${TPtest}`, true); // parameter skal være her! efter "posts/"
+    xhttp.open('GET', `${WPurl}posts/${InfoPost}`, true); // parameter skal være her! efter "posts/"
     xhttp.setRequestHeader('Authorization', `bearer ${WPkey}`);
     xhttp.send();
     
@@ -39,17 +43,44 @@ function getDataWP(TPtest){
 
 
 //  xhttp.open('GET', `${WPurl}?api_key=${WPkey}`, true);
-function renderWP(data){ // billede
-    document.querySelector('.left section.informationimg').innerHTML = `
-    <img src="${data.acf.info_image.url}">
-    
-    
-`;
-document.querySelector('.left section.informationarticle').innerHTML = `
-<h1>${data.acf.info_header}</h1>
-<p>${data.acf.info_text}</p>
+function renderInfoWP(data){ // billede
+    document.querySelector('.info-gen').innerHTML = `
 
+                        <div class="infocontainer">
+                        <div class="left">
+                            <section class="informationimg">
+                                <img src="${data.acf.info_image_left.url}" alt="">
+                            </section>
+                            <section class="informationarticle">
+                                <article><h1>${data.acf.info_header_left}</h1>
+                                <p>${data.acf.info_text_left}</p>
+                       </article>
+                            </section>
+                        </div>
+
+                        <div class="mid">
+                            <section class="informationarticle">
+                                <article><h1>${data.acf.info_header_mid}</h1>
+                                <p>${data.acf.info_text_mid}</p></article>
+                            </section>
+                            <section class="informationimg">
+                                <img src="${data.acf.info_image_mid.url}" alt="">
+                            </section>
+                        </div>
+                        
+                        <div class="right">
+                            <section class="informationimg">
+                                <img src="${data.acf.info_image_right.url}" alt="">
+                            </section>
+                            <section class="informationarticle">
+                                <article><h1>${data.acf.info_header_right}</h1>
+                                <p>${data.acf.info_text_right}</p></article>
+                            </section>
+                        </div>
+                    </div>
+    
 `;
+
 }
 
 
